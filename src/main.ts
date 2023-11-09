baf3651449340bd8b8abaf8014e87a8a1bdfc48f
Rafael Olivares <olrafael@mailfence.com>
2023-11-09 14:58:45 -0300

    Explainers

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,31 @@
 import { unsplashCredits } from "./constants";
 import { initialize } from "./functions/start";
+import {
+  EXPLAINER,
+  INITIAL_DETAIL_CAPTION,
+  PAGE_TITLE,
+  SUB_TITLE,
+} from "./strings";
 import "./style.css";
 
 const [author, site] = unsplashCredits;
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
 <div id="content">
-<h1>Neymarmeter</h1>
-<h3>Buscando referências a Neymar na mídia brasileira</h3>
+<h1>${PAGE_TITLE}</h1>
+<h3>${SUB_TITLE}</h3>
 <div id="chart"></div>
-<div id="detail">Explore o gráfico para ver detalhes</div>
+<div id="detail">${INITIAL_DETAIL_CAPTION}</div>
 <div id="site-list"></div>
 <div id="info">
-  <h3>Como funciona</h3>
+  <h3>${EXPLAINER.title}</h3>
   <p>
-    Todos os dias, por volta das 3h, 9h, 15h e 21h (de Brasília), nosso
-    robô abre a página inicial dos principais sites de notícias do Brasil.
-    Se o robô encontra o termo "Neymar", cria uma entrada no banco de
-    dados.<br /><br />
-    O gráfico mostra em quantos sites encontramos "Neymar" na página
-    principal. Quanto mais claro o verde, mais sites tinham o termo em sua
-    página naquele momento. Sendo assim, também não contamos o número de
-    menções em cada página, apenas se havia alguma menção ou não.
+    ${EXPLAINER.robot}<br /><br />
+    ${EXPLAINER.chart}<br /><br />
+    ${EXPLAINER.clarifier}
   </p>
   <p>
-    O código está disponível no
-    <a
-      href="https://github.com/olrafa/neymarmeter"
-      target="_blank"
-      rel="noopener noreferrer"
-      >Github</a
-    >.
+    ${EXPLAINER.code}
   </p>
 </div>
 </div>
---
+++ src/strings/index.ts
@@ -0,0 +1,24 @@
+export const PAGE_TITLE = "Neymarmeter";
+export const SUB_TITLE = "Buscando referências a Neymar na mídia brasileira";
+export const INITIAL_DETAIL_CAPTION = "Explore o gráfico para ver detalhes";
+
+export const EXPLAINER = {
+  title: "Como funciona",
+  robot: `Todos os dias, por volta das 3h, 9h, 15h e 21h (de Brasília), nosso
+    robô abre a página inicial dos principais sites de notícias do Brasil.
+    Se o robô encontra o termo "Neymar", cria uma entrada no banco de
+    dados.`,
+  chart: `O gráfico mostra em quantos sites encontramos "Neymar" na página
+    principal. Quanto mais claro o verde, mais sites tinham o termo em sua
+    página naquele momento.`,
+  clarifier: `Não contamos quantas menções existiam em cada site. A contagem 
+  é se o site traz alguma menção ou não. Mesmo se existirem menções em editorias 
+  diferentes (esportes e celebridades, por exemplo), cada site conta uma vez só.`,
+  code: ` O código está disponível no
+    <a
+      href="https://github.com/olrafa/neymarmeter"
+      target="_blank"
+      rel="noopener noreferrer"
+      >Github</a
+    >.`,
+};
